38183cf215d36652ae416fe8ea8ca06b918533f2
komal <chahliakomal777@gmail.com>
2021-05-31 17:26:51 +0530

    website

---
+++ index.js
@@ -0,0 +1,8 @@
+function slideshow() {
+  var x = document.getElementById('check');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
